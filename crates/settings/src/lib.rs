2e84530964c8c5d68d6d06e6a22d3d958662b402
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-02-15 20:38:27 +0530

    feat: add hierarchiral database field toggles to AppConfig

--- crates/settings/src/lib.rs
+++ crates/settings/src/lib.rs
@@ -13,8 +13,148 @@ pub struct AppConfig {
     pub block: Block,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Block {}
+#[derive(Deserialize, Debug, Default)]
+pub struct Block {
+    pub hash: bool,
+    pub number: bool,
+    pub size: bool,
+    pub detail_level: bool,
+    pub ver: bool,
+    pub system_calls: bool,
+    pub header: BlockHeader,
+    pub uncles: EnabledOnly,
+    pub transaction_traces: TransactionTraces,
+    pub balance_changes: EnabledOnly,
+    pub code_changes: CodeChanges,
+    pub withdrawals: Withdrawals,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct BlockHeader {
+    pub enabled: bool,
+    pub parent_hash: bool,
+    pub uncle_hash: bool,
+    pub coinbase: bool,
+    pub state_root: bool,
+    pub transactions_root: bool,
+    pub receipt_root: bool,
+    pub logs_bloom: bool,
+    pub difficulty: bool,
+    pub total_difficulty: bool,
+    pub number: bool,
+    pub gas_limit: bool,
+    pub gas_used: bool,
+    pub timestamp: bool,
+    pub extra_data: bool,
+    pub mix_hash: bool,
+    pub nonce: bool,
+    pub hash: bool,
+    pub base_fee_per_gas: bool,
+    pub withdrawals_root: bool,
+    pub tx_dependency: bool,
+    pub blob_gas_used: bool,
+    pub excess_blob_gas: bool,
+    pub parent_beacon_root: bool,
+    pub requests_hash: bool,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct TransactionTraces {
+    pub enabled: bool,
+    pub to: bool,
+    pub nonce: bool,
+    pub gas_price: bool,
+    pub gas_limit: bool,
+    pub value: bool,
+    pub input: bool,
+    pub v: bool,
+    pub r: bool,
+    pub s: bool,
+    pub gas_used: bool,
+    pub r#type: bool, // 'type' is a reserved keyword in Rust
+    pub max_fee_per_gas: bool,
+    pub max_priority_fee_per_gas: bool,
+    pub index: bool,
+    pub hash: bool,
+    pub from: bool,
+    pub return_data: bool,
+    pub public_key: bool,
+    pub begin_ordinal: bool,
+    pub end_ordinal: bool,
+    pub status: bool,
+    pub blob_gas: bool,
+    pub blob_gas_fee_cap: bool,
+    pub blob_hashes: bool,
+    pub receipt: Receipt,
+    pub calls: Calls,
+    pub set_code_authorizations: EnabledOnly,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct Receipt {
+    pub enabled: bool,
+    pub state_root: bool,
+    pub cumulative_gas_used: bool,
+    pub logs_bloom: bool,
+    pub blob_gas_used: bool,
+    pub blob_gas_price: bool,
+    pub logs: EnabledOnly,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct Calls {
+    pub enabled: bool,
+    pub index: bool,
+    pub parent_index: bool,
+    pub depth: bool,
+    pub call_type: bool,
+    pub caller: bool,
+    pub address: bool,
+    pub address_delegates_to: bool,
+    pub value: bool,
+    pub gas_limit: bool,
+    pub gas_consumed: bool,
+    pub return_data: bool,
+    pub input: bool,
+    pub executed_code: bool,
+    pub suicide: bool,
+    pub keccak_preimages: bool,
+    pub status_failed: bool,
+    pub status_reverted: bool,
+    pub failure_reason: bool,
+    pub state_reverted: bool,
+    pub begin_ordinal: bool,
+    pub end_ordinal: bool,
+    pub storage_changes: EnabledOnly,
+    pub balance_changes: EnabledOnly,
+    pub nonce_changes: EnabledOnly,
+    pub gas_changes: EnabledOnly,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct CodeChanges {
+    pub enabled: bool,
+    pub address: bool,
+    pub old_hash: bool,
+    pub old_code: bool,
+    pub new_hash: bool,
+    pub new_code: bool,
+    pub ordinal: bool,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct Withdrawals {
+    pub enabled: bool,
+    pub index: bool,
+    pub validator_index: bool,
+    pub address: bool,
+    pub amount: bool,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct EnabledOnly {
+    pub enabled: bool,
+}
 
 impl AppConfig {
     pub fn new() -> Result<Self, ConfigError> {
