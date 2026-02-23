dc200d72967a098f8c62af208be46256bbf718bc
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-02-24 02:42:34 +0530

    feat: add complete impl Display for ExtractedTransactionTraces and its inner structs

--- crates/settings/src/extract.rs
+++ crates/settings/src/extract.rs
@@ -1119,9 +1119,268 @@ impl fmt::Display for ExtractedBlock {
             writeln!(f, "  ],")?;
         }
 
+        if let Some(ref traces) = self.transaction_traces {
+            writeln!(f, "  transaction_traces: [")?;
+            for trace in traces {
+                let trace_str = format!("  {}", trace).replace('\n', "\n  ");
+                writeln!(f, "{},", trace_str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+
         write!(f, "}}")
     }
 }
+
+impl fmt::Display for ExtractedTransactionTraces {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "TransactionTrace {{")?;
+
+        macro_rules! display_field {
+            ($field_name:expr, $value:expr) => {
+                if let Some(ref v) = $value {
+                    writeln!(f, "  {}: {:?},", $field_name, v)?;
+                }
+            };
+        }
+
+        display_field!("to", self.to);
+        display_field!("nonce", self.nonce);
+        display_field!("gas_price", self.gas_price);
+        display_field!("gas_limit", self.gas_limit);
+        display_field!("value", self.value);
+        display_field!("input", self.input);
+        display_field!("v", self.v);
+        display_field!("r", self.r);
+        display_field!("s", self.s);
+        display_field!("gas_used", self.gas_used);
+        display_field!("type", self.r#type);
+        display_field!("max_fee_per_gas", self.max_fee_per_gas);
+        display_field!("max_priority_fee_per_gas", self.max_priority_fee_per_gas);
+        display_field!("index", self.index);
+        display_field!("hash", self.hash);
+        display_field!("from", self.from);
+        display_field!("return_data", self.return_data);
+        display_field!("public_key", self.public_key);
+        display_field!("begin_ordinal", self.begin_ordinal);
+        display_field!("end_ordinal", self.end_ordinal);
+        display_field!("status", self.status);
+
+        display_field!("blob_gas", self.blob_gas);
+        display_field!("blob_gas_fee_cap", self.blob_gas_fee_cap);
+        display_field!("blob_hashes", self.blob_hashes);
+
+        if let Some(ref v) = self.access_list {
+            writeln!(f, "  access_list: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.receipt {
+            let str = format!("  {}", v).replace('\n', "\n  ");
+            writeln!(f, "  receipt: {},", str)?;
+        }
+        if let Some(ref v) = self.calls {
+            writeln!(f, "  calls: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.set_code_authorizations {
+            writeln!(f, "  set_code_authorizations: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+
+        write!(f, "}}")
+    }
+}
+
+impl fmt::Display for ExtractedAccessTuple {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "AccessTuple {{")?;
+        if let Some(ref v) = self.address {
+            writeln!(f, "  address: {:?},", v)?;
+        }
+        if let Some(ref v) = self.storage_keys {
+            writeln!(f, "  storage_keys: {:?},", v)?;
+        }
+        write!(f, "}}")
+    }
+}
+
+impl fmt::Display for ExtractedTransactionReceipt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "TransactionReceipt {{")?;
+        macro_rules! display_field {
+            ($field_name:expr, $value:expr) => {
+                if let Some(ref v) = $value {
+                    writeln!(f, "  {}: {:?},", $field_name, v)?;
+                }
+            };
+        }
+
+        display_field!("state_root", self.state_root);
+        display_field!("cumulative_gas_used", self.cumulative_gas_used);
+        display_field!("logs_bloom", self.logs_bloom);
+
+        if let Some(ref v) = self.logs {
+            writeln!(f, "  logs: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+
+        display_field!("blob_gas_used", self.blob_gas_used);
+        display_field!("blob_gas_price", self.blob_gas_price);
+
+        write!(f, "}}")
+    }
+}
+
+impl fmt::Display for ExtractedLog {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Log {{")?;
+        macro_rules! display_field {
+            ($field_name:expr, $value:expr) => {
+                if let Some(ref v) = $value {
+                    writeln!(f, "  {}: {:?},", $field_name, v)?;
+                }
+            };
+        }
+
+        display_field!("address", self.address);
+        display_field!("topics", self.topics);
+        display_field!("data", self.data);
+        display_field!("index", self.index);
+        display_field!("block_index", self.block_index);
+        display_field!("ordinal", self.ordinal);
+
+        write!(f, "}}")
+    }
+}
+
+impl fmt::Display for ExtractedTracsactionTraceCall {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "TransactionTraceCall {{")?;
+
+        macro_rules! display_field {
+            ($field_name:expr, $value:expr) => {
+                if let Some(ref v) = $value {
+                    writeln!(f, "  {}: {:?},", $field_name, v)?;
+                }
+            };
+        }
+
+        display_field!("index", self.index);
+        display_field!("parent_index", self.parent_index);
+        display_field!("depth", self.depth);
+        display_field!("call_type", self.call_type);
+        display_field!("caller", self.caller);
+        display_field!("address", self.address);
+        display_field!("address_delegates_to", self.address_delegates_to);
+        display_field!("value", self.value);
+        display_field!("gas_limit", self.gas_limit);
+        display_field!("gas_consumed", self.gas_consumed);
+        display_field!("return_data", self.return_data);
+        display_field!("input", self.input);
+        display_field!("executed_code", self.executed_code);
+        display_field!("suicide", self.suicide);
+        display_field!("keccak_preimages", self.keccak_preimages);
+        display_field!("status_failed", self.status_failed);
+        display_field!("status_reverted", self.status_reverted);
+        display_field!("failure_reason", self.failure_reason);
+        display_field!("state_reverted", self.state_reverted);
+        display_field!("begin_ordinal", self.begin_ordinal);
+        display_field!("end_ordinal", self.end_ordinal);
+
+        if let Some(ref v) = self.storage_changes {
+            writeln!(f, "  storage_changes: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.balance_changes {
+            writeln!(f, "  balance_changes: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.nonce_changes {
+            writeln!(f, "  nonce_changes: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.code_changes {
+            writeln!(f, "  code_changes: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.gas_changes {
+            writeln!(f, "  gas_changes: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+        if let Some(ref v) = self.account_creations {
+            writeln!(f, "  account_creations: [")?;
+            for item in v {
+                let str = format!("      {}", item).replace('\n', "\n      ");
+                writeln!(f, "{},", str)?;
+            }
+            writeln!(f, "  ],")?;
+        }
+
+        write!(f, "}}")
+    }
+}
+
+impl fmt::Display for ExtractedSetCodeAuthorization {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "SetCodeAuthorization {{")?;
+        macro_rules! display_field {
+            ($field_name:expr, $value:expr) => {
+                if let Some(ref v) = $value {
+                    writeln!(f, "  {}: {:?},", $field_name, v)?;
+                }
+            };
+        }
+
+        display_field!("discarded", self.discarded);
+        display_field!("chain_id", self.chain_id);
+        display_field!("address", self.address);
+        display_field!("nonce", self.nonce);
+        display_field!("v", self.v);
+        display_field!("r", self.r);
+        display_field!("s", self.s);
+        display_field!("authority", self.authority);
+
+        write!(f, "}}")
+    }
+}
+
+
 
 impl fmt::Display for ExtractedBlockHeader {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
